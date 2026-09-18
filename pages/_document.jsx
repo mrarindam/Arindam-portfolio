6f4a5365c10f10f555957d58f3c554658a8acb7c
mrarindam <marindam342@gmail.com>
2026-09-19 01:42:15 +0530

    adsense purge

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,6 +6,12 @@ export default function Document() {
       <Head>
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="theme-color" content="#0a0a0a" />
+        <meta name="google-adsense-account" content="ca-pub-4835378757253642" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4835378757253642"
+          crossOrigin="anonymous"
+        />
         {/* Disable browser scroll restoration so GSAP/Lenis control scroll position */}
         <script
           dangerouslySetInnerHTML={{
